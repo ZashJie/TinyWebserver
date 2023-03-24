92b3c58e4430c84e463fbc0dd338cd77502ce084
ZashJie <1030298236@qq.com>
2023-03-24 23:58:10 +0800

    11

--- log.cpp
+++ log.cpp
@@ -67,7 +67,7 @@ bool Log::init(const char *file_name, int close_log, int log_buf_size, int split
 
     return true;
 }
-
+//
 void Log::write_log(int level, const char *format, ...)
 {
     struct timeval now = {0, 0};
